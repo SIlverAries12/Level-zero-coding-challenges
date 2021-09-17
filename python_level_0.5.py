eb2f7d923326b807320b34bc4712db65c3a979fb
Bradley <zainbradleyblack@gmail.com>
2021-09-17 14:05:26 +0200

    fixed formulae second time

--- python_level_0.5.py
+++ python_level_0.5.py
@@ -1,8 +1,7 @@
-import math
 
 def area (a,b,c):
    s = 0.5*(a + b + c)
-   output = math.sqrt(s*((s-a)*(s-b)*(s-c)))
+   output = (s*((s-a)*(s-b)*(s-c))) ** 0.5
    print(str(output))
 
 area(3, 4, 5) 
